9fdcfa32769743ec278d642dea35fbcd257b05dd
kristerevensen <67630525+kristerevensen@users.noreply.github.com>
2020-12-10 18:38:38 +0100

    added buttons to campaigns

--- resources/views/partials/campaigns/new.blade.php
+++ resources/views/partials/campaigns/new.blade.php
@@ -6,7 +6,18 @@
         <div class="row">
             
             <div class="col-md-6 grid-margin stretch-card">
-    
+                <a href="{{ route('campaigns.new_bulk_links') }}" class="btn btn-sm btn-secondary  btn-icon-text">
+                    <i class="btn-icon-prepend" data-feather="plus"></i>
+                    {{ __('Bulk Link Creator') }}
+                </a>
+                <a href="{{ route('campaigns.new_template') }}" class="btn btn-sm btn-secondary  btn-icon-text">
+                    <i class="btn-icon-prepend" data-feather="plus"></i>
+                    {{ __('New Template') }}
+                </a>
+                <a href="{{ route('campaigns.new_link') }}" class="btn btn-sm btn-success  btn-icon-text">
+                    <i class="btn-icon-prepend" data-feather="plus"></i>
+                    {{ __('New Link') }}
+                </a>
             </div>
         </div>
     </div>
