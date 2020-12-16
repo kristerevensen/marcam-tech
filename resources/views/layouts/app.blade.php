669d5bcecd65c7663afda906c423dc6de2ffcb81
kristerevensen <67630525+kristerevensen@users.noreply.github.com>
2020-12-16 01:45:04 +0100

    moved clicks from marcam to marcam tech

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    @include('partials.gtm_head')
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,6 +17,7 @@
 
 </head>
 <body id="app">
+ @include('partials.gtm_body')
     <div class="main-wrapper">
 		<div class="horizontal-menu">
            @include('partials.topnav')
