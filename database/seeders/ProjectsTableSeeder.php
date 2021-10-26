b5720ef74f51b3c70eb4d8f865ce3ba2351c127f
kristerevensen <67630525+kristerevensen@users.noreply.github.com>
2021-10-27 01:34:30 +0200

    foreign keys fix templates

--- database/seeders/ProjectsTableSeeder.php
+++ database/seeders/ProjectsTableSeeder.php
@@ -15,7 +15,7 @@ class ProjectsTableSeeder extends Seeder
      */
     public function run()
     {
-        //DB::table('projects')->truncate();
+        DB::table('projects')->truncate();
 
         DB::table('projects')->insert([
             'project_name' => 'OneCall',
