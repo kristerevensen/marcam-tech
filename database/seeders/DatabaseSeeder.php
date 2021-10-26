dccaed6f977c4d221532a8d366a814f1d9baceba
kristerevensen <67630525+kristerevensen@users.noreply.github.com>
2021-10-27 01:39:13 +0200

    foreign keys fix templates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,7 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         $this->call([
             
